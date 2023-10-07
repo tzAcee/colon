88f1fec656753f7e3772ab8ac7f6ce91301d35d4
tzAcee <maximleis3@gmail.com>
2023-10-07 19:14:54 +0200

    print ast

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,26 @@
-#include <ColonCompiler.h>
+﻿#include <ColonCompiler.h>
 #include <iostream>
+
+void printTree(const std::string& prefix, ASTNode& node, bool isLeft)
+{
+	if (&node == nullptr) {
+		return;
+	}
+
+	std::cout << prefix;
+	std::cout << "|--";
+	std::cout << node.Identifier() << std::endl;
+
+	for (size_t i = 0; i < node.Children().size(); ++i) {
+		printTree(prefix + (isLeft ? "|   " : "    "), node.Children()[i], i < node.Children().size() - 1);
+	}
+}
+
+void printAST(ColonAST& ast)
+{
+	printTree("", ast.Head(), false);
+}
+
 int main()
 {
 
@@ -11,8 +32,13 @@ int main()
 		std::cin >> src;
 		std::cout << "\n";
 		auto res = compiler.compile(src);
-		if(res.has_value())
-			std::cerr << "\nSuccess! \n";
+		if (res.has_value())
+		{
+			std::cerr << "\nSuccess!\n";
+			auto ast = res.value().AST;
+			printAST(ast);
+		}
+
 	}
 
 	return 0;
